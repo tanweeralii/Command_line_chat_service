aa66c08046bc3f3f979ae56f1eca27b5d284a3fd
tanweeralii <46401579+tanweeralii@users.noreply.github.com>
2019-07-09 00:04:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -24,49 +24,49 @@ def socket_accept():
     del addresses[:]
     while True:
         conn, address = s.accept()
-        s.setblocking(1)
         connections.append(conn)
         addresses.append(addresses)
 
-def receive_commands(connections):
-    while True:
-        for conn in connections:
-            data=conn.recv(1024)
-            string=str(data[:].decode("utf-8"))
-            length=str(len(connections))
-            if string=='@TanweerAli#:~' + 'list':
-                conn.send(str.encode(length))
-            else:
-                for i in connections:
-                    if i==conn:
-                        continue
-                    else:
-                        i.send(str.encode(string))
-
-def list_connections(connections):
+def receive_commands():
     while True:
         for i,j in enumerate(connections):
             try:
-                j.send(str.encode(' '))
+                j.send(str.encode(''))
                 j.recv(20480)
             except:
                 del connections[i]
                 del addresses[i]
                 continue
-
-
+        for i,conn in enumerate(connections):
+            data = conn.recv(1024)
+            string=str(data[:].decode("utf-8"))
+            loguname = string.split('@')
+            log = loguname[1]
+            log1 = log.split('#')
+            log2 = log1[0]
+            if string == '@' + log2 + '#:~ ' + 'list':
+                conn.send(str.encode(str(len(connections)))
+            elif string == '@' + log2 + '#:~ ' + 'quit':
+                 send(str.encode("YOU ARE OUT"))  conn.
+                 conn.close()
+                 del connections[i]
+                 del addresses[i]
+            else:
+                for i in connections:
+                    if i==conn:
+                        continue
+                    else:
+                        i.send(str.encode(string)
+                               
 def work(j):
     if j==0:
         create_socket()
         bind_socket()
         socket_accept()
     elif j==1:
-        receive_commands(connections)
-    elif j==2:
-        list_connections(connections)
+        receive_commands()
 
-
-for j in range(3):
+for j in range(2):
     t=threading.Thread(target=work, args=(j,))
     t.start()
 
